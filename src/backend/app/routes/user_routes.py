91129b25276527fd5181a0e9c8fb785f624979ca
David Safran <dsafran@strategicstaff.com>
2025-02-23 14:42:19 +0800

    Simplify to html front end

--- src/backend/app/routes/user_routes.py
+++ src/backend/app/routes/user_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 from app.controllers.user_controller import UserController
 
 user_bp = Blueprint('user', __name__)
@@ -6,3 +6,7 @@ user_bp = Blueprint('user', __name__)
 user_bp.route('/users', methods=['GET'])(UserController.get_all_users)
 user_bp.route('/users/<int:user_id>', methods=['GET'])(UserController.get_user)
 user_bp.route('/users', methods=['POST'])(UserController.create_user)
+
+@user_bp.route('/test', methods=['GET'])
+def view_users():
+    return render_template('users.html')
